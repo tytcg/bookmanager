9e1c4d1e66383a876e30c9cc7721db0e779bd124
tyt <tyt>
2021-07-28 21:29:05 +0800

    book的正式版

---
+++ book/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class BookInfo(models.Model):
+    name = models.CharField(max_length=10,unique=True)
+    pub_date = models.DateField(null=True)
+    readcount = models.IntegerField(default=0)
+    commentcount = models.IntegerField(default=0)
+    is_delete = models.BooleanField(default=False)
+
+
+    class Meta:
+        db_table = "bookinfo"
+        verbose_name = "书籍管理"
+
+
+class PersonInfo(models.Model):
+    GENDER_CHOICE = [
+        (1, 'male'),
+        (2, 'female')
+    ]
+    name = models.CharField(max_length=10,unique=True)
+    gender = models.IntegerField(choices=GENDER_CHOICE)
+    description = models.CharField(max_length=100,null=True)
+    is_delete = models.BooleanField(default=False)
+    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "personinfo"
