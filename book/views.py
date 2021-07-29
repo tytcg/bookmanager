da651a6b92e94fbe9f3c8ee8fa73ee91e4edf555
tyt <tyt>
2021-07-29 09:36:10 +0800

    数据库的增删改操作

--- book/views.py
+++ book/views.py
@@ -6,7 +6,40 @@ from book.models import BookInfo
 
 
 def index(request):
-
     obj = BookInfo.objects.all()
     print(obj)
     return HttpResponse("ok")
+
+
+################增加数据###################
+
+from book.models import BookInfo
+
+book = BookInfo(
+    name="必然",
+    pub_date="2010-1-6"
+)
+# 必须要调用函数才可以实现增加
+book.save()
+# 方法二
+BookInfo.objects.create(
+    name="java",
+    pub_date="2020-1-17",
+    readcount=10,
+    commentcount=2
+)
+
+################修改数据####################
+# 方法一
+book = BookInfo.objects.get(id=6)
+book.name = "python入门"
+book.readcount = 1
+book.save()
+# 方法二
+BookInfo.objects.filter(id=7).update(name="django从基础到高级", readcount=200)
+#################删除数据###################
+# 方法一
+book = BookInfo.objects.get(id=6).delete()
+# 方法二
+BookInfo.objects.filter(id).delete()
+
